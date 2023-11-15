4987ea266876681e60672a6e06a7f85d648764c9
Shayan Halder <shayanhalder@dhcp-10-8-060-059.mobile.reshsg.uci.edu>
2023-11-15 00:14:43 -0800

    added react components with outline/skeleton of how data would work with state logic

--- client/react-app/src/App.jsx
+++ client/react-app/src/App.jsx
@@ -1,33 +1,82 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+function HabitList( {currentHabitData }) {
+  const [currentHabits, setCurrentHabits] = useState(currentHabitData)
+  // if the data for today's habits is changed (either by user adding/deleting the )
+  // tasks shown, then the list will re-render to reflect that change. 
+
+  const socialHabits = currentHabits['social'].map((habit) => {
+    return <Habit title={habit['title']} frequency={habit['frequency']} description={habit['description']} setCurrentHabits={setCurrentHabits}> </Habit>
+  })
+
+  const academicHabits = currentHabits['academic'].map((habit) => {
+    return <Habit title={habit['title']} frequency={habit['frequency']} description={habit['description']} setCurrentHabits={setCurrentHabits}> </Habit>
+  })
+
+  const personalHabits = currentHabits['personal'].map((habit) => {
+    return <Habit title={habit['title']} frequency={habit['frequency']} description={habit['description']} setCurrentHabits={setCurrentHabits}> </Habit>
+  })
+
+  return (
+    <div>
+      <div> Social {socialHabits}</div>
+      <div> Academic {academicHabits}</div>
+      <div> Personal {personalHabits}</div>
+    </div>
+
+  )
+
+}
+
+function Habit({title, frequency, description, setCurrentHabits }) {
+  const [title, setTitle] = useState(title)
+  const [frequency, setFrequency] = useState(frequency)
+  const [description, setDescription] = useState(description)
+  const [editMode, setEditMode] = useState(false) // controls whether the pop-up modal for editing the tasks in the list is being displayed currently or not
+
+  return (
+    <div>
+      <div>
+        {/* <button> </button> */}
+        {/* button for checking off if the task was completed */}
+        
+      </div>
+      <div> 
+        {/* div containing the task name and frequency */}
+          <div>
+            {title}
+          </div>
+          <div>
+            {frequency} 
+            {/* formatted in the tag-like display with css */}
+          </div>
+      </div>
+      <div> 
+        {/* <button> </button> */}
+        {/* right-arrow button for expanding into pop up modal to edit task */}
+        {/* button changes the editMode state to True, causing the pop up  */}
+        {/* modal to appear where task can be edited.  */}
+
+        {/* pop up modal will be in another react component */}
+        {/* {editMode == true ? <PopUpModal title={title}, etc../> : <>} */}
+        {/* above displays Pop up modal if the edit mode state is true  */}
+      </div>
+    </div>
+  )
+  
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HabitList currentHabitData={currentHabitData}/>
+      {/* currentHabitData will be retrieved from MongoDB database, where all the user's habits (daily/weekly, etc...) are stored  */}
+      
     </>
   )
 }
