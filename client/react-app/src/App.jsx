d92d4b7344687233906140727f106bf320a2e32e
Shayan Halder <shayanhalder@dhcp-172-31-181-067.mobile.uci.edu>
2023-11-15 20:49:05 -0800

    added node.js backend boilerplate

--- client/react-app/src/App.jsx
+++ client/react-app/src/App.jsx
@@ -7,7 +7,7 @@ function HabitList( {currentHabitData }) {
   const [currentHabits, setCurrentHabits] = useState(currentHabitData)
   // if the data for today's habits is changed (either by user adding/deleting the )
   // tasks shown, then the list will re-render to reflect that change. 
-
+ 
   const socialHabits = currentHabits['social'].map((habit) => {
     return <Habit title={habit['title']} frequency={habit['frequency']} description={habit['description']} setCurrentHabits={setCurrentHabits}> </Habit>
   })
--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
 // Node backend
+
+const PORT = 3001
+const mongoose = require('mongoose')
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+// connect to databse
+mongoose.connect('mongodb+srv://shalder:rishi1105@treejam.p584kjj.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => {
+    console.log('connected to database')
+})
