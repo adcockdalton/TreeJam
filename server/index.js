705836774ecef4f47c64734876708d6fd2aa73df
Dalton Adcock <dadcock@uci.edu>
2023-11-15 21:36:50 -0800

    nodemon, tests written for localhost:3001

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@
 
 const PORT = 3001
 const mongoose = require('mongoose')
+const cors = require('cors')
 const express = require('express')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 // connect to databse
 mongoose.connect('mongodb+srv://shalder:rishi1105@treejam.p584kjj.mongodb.net/?retryWrites=true&w=majority', {
@@ -15,4 +17,8 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => {
     console.log('connected to database')
+})
+
+app.get("/test", (req, res) => {
+    res.json({message: "Hello World!"})
 })
