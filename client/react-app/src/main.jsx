1ee9044e53d6898bfae744f4a7d320667e4352bf
Shayan Halder <shayanhalder@dhcp-10-8-032-134.mobile.reshsg.uci.edu>
2023-11-17 08:41:24 -0800

    removed chakra ui preset css

--- client/react-app/src/main.jsx
+++ client/react-app/src/main.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: "",
+      },
+    }),
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ChakraProvider resetCSS={false} theme={theme}>
       <App />
+    </ChakraProvider>
+
   </React.StrictMode>,
   
 )
